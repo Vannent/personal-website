5e940ab5099f1785395ddf0b77973e287ac8cdc0
Roberto Cedeno <98601749+Vannent@users.noreply.github.com>
2022-05-19 20:52:39 -0700

    feat: add ref and handle funcs

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,11 +1,51 @@
-import React, { createContext, useContext, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useRef,
+  forwardRef,
+} from "react";
 
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
   const [showTheme, setShowTheme] = useState(true);
+  const aboutRef = useRef();
+  const homeRef = useRef();
+  const projectsRef = useRef();
+  const contactRef = useRef();
+
+  const handleAboutClick = () => {
+    aboutRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleHomeClick = () => {
+    homeRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleProjectsClick = () => {
+    projectsRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleContactClick = () => {
+    contactRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <Context.Provider value={{ showTheme, setShowTheme }}>
+    <Context.Provider
+      value={{
+        showTheme,
+        setShowTheme,
+        aboutRef,
+        handleAboutClick,
+        homeRef,
+        handleHomeClick,
+        projectsRef,
+        contactRef,
+        handleContactClick,
+        handleProjectsClick,
+      }}
+    >
       {children}
     </Context.Provider>
   );
